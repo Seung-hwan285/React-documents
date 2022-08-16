d4cd1f249a32b3855bf13d974e5306cf0d504924
승환 <ghks123285@gmail.com>
2022-08-16 10:43:30 +0900

    docs : ch3 README 작성

---
+++ ch3/src/ch3-1/UseReducer.jsx
@@ -0,0 +1,34 @@
+import { useReducer } from "react";
+
+const initialState = {count: 0};
+
+function reduce1(state, action) {
+  switch (action.type) {
+    case 'increment':
+      return {count: state.count + 1};
+    case 'decrement':
+      return {count: state.count - 1};
+
+    case 'divsion':
+        
+        return {count: state.count / 2};
+    default:
+      throw new Error();
+  }
+
+}
+
+
+function Counter() {
+  const [state, dispatch] = useReducer(reduce1, initialState);
+  
+  return (
+    <>
+      Count: {state.count}
+      <button onClick={() => dispatch ({type: 'decrement'})}>-</button>
+      <button onClick={() => dispatch ({type: 'increment'})}>+</button>
+        <button onClick={()=> dispatch ({type : 'divsion'})}>/</button>
+    </>
+  );
+}
+export default Counter;
